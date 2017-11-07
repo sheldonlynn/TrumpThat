275637dc4f6db1da700d466a78d0560f7e9d884f
Sheldon Lynn <sheldonlynn@outlook.com>
2017-11-06 23:22:03 -0800

    ui

--- client/components/InputBox.js
+++ client/components/InputBox.js
@@ -1,34 +1,25 @@
 import React from 'react';
-import axios from 'axios';
+import { Animate } from 'react-move';
+
+const inputStyle = {
+
+}
 
 export default class InputBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ""};
-    this.handleChange = this.handleChange.bind(this);
-    this.trumpify = this.trumpify.bind(this);
-  }
-
-  trumpify(event) {
-    // axios.post(this.props.url)
-    console.log(this.state.value);
-  }
-
-  handleChange(e) {
-    this.setState({ value: e.target.value });
   }
 
   render() {
     return (
-      <div>
+      <div className={"input-box " + (this.props.display ? 'show' : 'hide')}>
           <textarea
             rows="5"
             cols="40"
-            value={this.state.value}
-            onChange={this.handleChange}>
-            Input your tweet
+            value={this.props.input}
+            onChange={this.props.handleChange}>
           </textarea>
-        <button onClick={(e) => this.trumpify(e)}>Trumpify</button>
+        <button onClick={this.props.trumpify}>Trumpify</button>
       </div>
     )
   }
--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -2,16 +2,51 @@
     ./client/components/App.jsx
  */
 import React from 'react';
+import axios from 'axios';
 import InputBox from '../components/InputBox';
 const image = require('../public/images/trump.gif');
+import { Animate } from 'react-move';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {input: "", trumped: "", displayInput: true};
+
+    this.url = 'http://localhost:3001/api/trumpthat';
+
+    this.trumpify = this.trumpify.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  trumpify() {
+    console.log("shit is happening!");
+    axios.post(this.url, {
+      phrase: this.state.input
+    })
+      .then((res) => {
+        this.setState({trumped: res.data.message, displayInput: false});
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }
+
+  handleChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
   render() {
     return (
       <div style={{textAlign: 'center'}}>
-        <img src={image} />
-        <h1>Trump That Tweet</h1>
-        <InputBox url='http://localhost:3001/api/trumpthat'/>
+        <h1>TRUMP THAT TWEET</h1>
+        <img src={image} id="trump-gif"/>
+        <InputBox
+          trumpify={this.trumpify}
+          input={this.state.input}
+          handleChange={this.handleChange}
+          display={this.state.displayInput}/>
+        { this.state.trumped }
       </div>
     )
   }
