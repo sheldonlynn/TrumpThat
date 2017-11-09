c94db81adde94a65d8f02e43b44592ba2ec43ed7
Sheldon Lynn <sheldonlynn@outlook.com>
2017-11-08 19:48:13 -0800

    Still frame for display mode and reduced animations

--- client/components/TrumpHead.js
+++ client/components/TrumpHead.js
@@ -1,11 +1,13 @@
-const head = require('../public/images/trump.gif');
+const headAnimated = require('../public/images/trump.gif');
+const headStill = require('../public/images/trump-still.gif');
 import React from 'react';
 
 export default class TrumpHead extends React.Component {
   render() {
+    let displayMode = this.props.mode === 'display';
     return (
-      <div className={"trump-head " + (this.props.mode === 'display' ? 'display' : '')}>
-        <img src={head} id="trump-gif"/>
+      <div className={"trump-head " + (displayMode ? 'display' : '')}>
+        <img src={displayMode ? headStill : headAnimated} id="trump-gif"/>
       </div>
     )
   }
